63637c86fcf3437a726e469c8293c4d8b99dd8c8
Luis Angel <tortoladev@gmail.com>
2019-10-29 14:43:08 -0600

    Reading image from path

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sudoku-solver
+
+##### Command to run script
+
+> python3 solver.py --p "/Users/pq5xttu8/Desktop/Tortolala/Repositories/sudoku-solver/sudoku.jpg"
--- solver.py
+++ solver.py
@@ -1,12 +1,15 @@
 import argparse
 import logging
+import cv2 as cv
 
 logging.basicConfig(level = logging.INFO)
 logger = logging.getLogger('sudoku-solver')
-
 
 parser = argparse.ArgumentParser(description='Sudoku solver')
 parser.add_argument('--p', type=str, required = True, help='path to image to evaluate')
 
 image_path = parser.parse_args().p
 logger.info(image_path)
+
+img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+logger.info(img.shape)
